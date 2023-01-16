a46b5506135000efc023ffc50aa72625d688c93b
Jérôme Abel <contact@jeromeabel.net>
2023-01-17 00:12:18 +0100

    up server : test db connexion

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'production'){
 const express = require("express")
 const mongoose = require('mongoose')
 const path = require('path')
-const cors = require('cors') // Dev Front/Back
+//const cors = require('cors') // Dev Front/Back
 
 // Import Router
 const argonautesRouter = require("./src/argonautes.routes");
@@ -18,15 +18,11 @@ const PORT = process.env.PORT;
 
 // App use : middlewares
 app.use(express.json());
-//if ( process.env.DB_IS_LOCAL )
-app.use(cors())
+//app.use(cors())
 app.use('/api', argonautesRouter) // Route API
 app.use(express.static(path.resolve(__dirname, '../frontend/dist'))); // Front-end
 
 // Database 
-//let uri = process.env.DB_ATLAS
-//if ( process.env.DB_IS_LOCAL ) uri = process.env.DB_LOCALHOST
-//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/argoBase?retryWrites=true&w=majority`;
 mongoose.set('strictQuery', false)
 mongoose.connect( process.env.DB_ATLAS ,  { useNewUrlParser: true, useUnifiedTopology: true } );
 const db = mongoose.connection;
@@ -34,7 +30,6 @@ db.on('error', (err) => console.error(err))
 db.once("open", function () {
   console.log("✓ Connected to Database");
 });
-
 
 // Server
 app.listen(PORT, function() {

1f2eefb70120e0962122d0608fdc066b881954df
Jérôme Abel <contact@jeromeabel.net>
2023-01-16 23:17:00 +0100

    up CORS and URI

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== 'production'){
 // Import libs
 const express = require("express")
 const mongoose = require('mongoose')
-const path = require('path');
+const path = require('path')
 const cors = require('cors') // Dev Front/Back
 
 // Import Router
@@ -18,12 +18,14 @@ const PORT = process.env.PORT;
 
 // App use : middlewares
 app.use(express.json());
-if ( process.env.DB_IS_LOCAL ) app.use(cors())
-app.use('/api', argonautesRouter)
-app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
+//if ( process.env.DB_IS_LOCAL )
+app.use(cors())
+app.use('/api', argonautesRouter) // Route API
+app.use(express.static(path.resolve(__dirname, '../frontend/dist'))); // Front-end
 
 // Database 
 let uri = process.env.DB_ATLAS
+uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_BASE}?retryWrites=true&w=majority`;
 if ( process.env.DB_IS_LOCAL ) uri = process.env.DB_LOCALHOST
 mongoose.set('strictQuery', false)
 mongoose.connect( uri ,  { useNewUrlParser: true, useUnifiedTopology: true } );
